581605ef199e1a307f1fff18bdf0e43a11eb0d3d
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2025-11-17 00:33:58 +0300

    implement home page TradingView Wedgets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,51 @@
-import { Button } from "@/components/ui/button"
+import TradingViewWidget from "@/components/features/TradingViewWidget";
+import { Button } from "@/components/ui/button";
+import {
+  HEATMAP_WIDGET_CONFIG,
+  MARKET_DATA_WIDGET_CONFIG,
+  MARKET_OVERVIEW_WIDGET_CONFIG,
+  TOP_STORIES_WIDGET_CONFIG,
+} from "@/lib/constants";
 
 const Home = () => {
+  const baseUrl = "https://s3.tradingview.com/external-embedding/embed-widget-";
+
   return (
     <div className="flex min-h-screen home-wrapper">
-      Home
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            className="custom-chart"
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            scriptUrl={`${baseUrl}market-overview.js`}
+            title="Market Overview"
+          />
+        </div>
+        <div className="md:col-span xl:col-span-2">
+          <TradingViewWidget
+            config={HEATMAP_WIDGET_CONFIG}
+            scriptUrl={`${baseUrl}stock-heatmap.js`}
+            title="Stock Heatmap"
+          />
+        </div>
+      </section>
+      <section className="grid w-full gap-8 home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            className="custom-chart"
+            config={TOP_STORIES_WIDGET_CONFIG}
+            scriptUrl={`${baseUrl}timeline.js`}
+          />
+        </div>
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <TradingViewWidget
+            config={MARKET_DATA_WIDGET_CONFIG}
+            scriptUrl={`${baseUrl}market-quotes.js`}
+          />
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
